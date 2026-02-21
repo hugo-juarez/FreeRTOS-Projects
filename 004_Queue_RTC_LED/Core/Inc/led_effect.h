401604ea181a134a93391cd67f8717ea13291c6f
Hugo Juarez <hsebastianjuarezp@gmail.com>
2026-02-21 16:57:29 -0600

    feat: Added API for init led timer and callback handler

--- 004_Queue_RTC_LED/Core/Inc/led_effect.h
+++ 004_Queue_RTC_LED/Core/Inc/led_effect.h
@@ -6,8 +6,15 @@
 #define INC_004_QUEUE_RTC_LED_LED_EFFECT_H
 
 #include <stdint.h>
+#include "FreeRTOS.h"
+#include "timers.h"
 
+// LED APIs
+void led_init_timer(void);
 void led_effect_stop(void);
 void led_effect(uint8_t effect);
+
+// LED Timer Callback
+void led_effect_callback(TimerHandle_t xTimer);
 
 #endif //INC_004_QUEUE_RTC_LED_LED_EFFECT_H
--- 004_Queue_RTC_LED/Core/Src/led_effect.c
+++ 004_Queue_RTC_LED/Core/Src/led_effect.c
@@ -4,12 +4,29 @@
 
 #include "led_effect.h"
 
+// Private global variables
+static TimerHandle_t led_timer_handle;
+static uint8_t led_effect_id;
+
+// LED APIs
+void led_init_timer(void)
+{
+    const TickType_t xDelay = 500 / portTICK_PERIOD_MS;
+    led_timer_handle = xTimerCreate("LED Timer", xDelay, pdTRUE, 0, led_effect_callback);
+}
+
 void led_effect_stop(void)
 {
 
 }
 
 void led_effect(uint8_t effect)
+{
+
+}
+
+// LED Timer Callback
+void led_effect_callback(TimerHandle_t xTimer)
 {
 
 }
