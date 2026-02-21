0ba72c29cf438c6da03d3d48788fe4c9fd4fa2df
Hugo Juarez <hsebastianjuarezp@gmail.com>
2026-02-21 16:15:34 -0600

    chore: Added led effects source and header

---
+++ 004_Queue_RTC_LED/Core/Inc/led_effect.h
@@ -0,0 +1,13 @@
+//
+// Created by hugo-juarez on 2/21/26.
+//
+
+#ifndef INC_004_QUEUE_RTC_LED_LED_EFFECT_H
+#define INC_004_QUEUE_RTC_LED_LED_EFFECT_H
+
+#include <stdint.h>
+
+void led_effect_stop(void);
+void led_effect(uint8_t effect);
+
+#endif //INC_004_QUEUE_RTC_LED_LED_EFFECT_H
---
+++ 004_Queue_RTC_LED/Core/Src/led_effect.c
@@ -0,0 +1,15 @@
+//
+// Created by hugo-juarez on 2/21/26.
+//
+
+#include "led_effect.h"
+
+void led_effect_stop(void)
+{
+
+}
+
+void led_effect(uint8_t effect)
+{
+
+}
