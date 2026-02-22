d53fd656f92dfdea991a0eb1f56b89f7d0e3ec27
Hugo Juarez <hsebastianjuarezp@gmail.com>
2026-02-22 13:24:29 -0600

    chore: Initialize rtc source and header files

---
+++ 004_Queue_RTC_LED/Core/Inc/rtc.h
@@ -0,0 +1,8 @@
+//
+// Created by hugo-juarez on 2/22/26.
+//
+
+#ifndef INC_004_QUEUE_RTC_LED_RTC_H
+#define INC_004_QUEUE_RTC_LED_RTC_H
+
+#endif //INC_004_QUEUE_RTC_LED_RTC_H
---
+++ 004_Queue_RTC_LED/Core/Src/rtc.c
@@ -0,0 +1,6 @@
+//
+// Created by hugo-juarez on 2/22/26.
+//
+
+#include "rtc.h"
+
