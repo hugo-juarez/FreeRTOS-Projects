b2a7e742a0f78c6b61390874f1bc6b08d4a752fa
Hugo Juarez <hsebastianjuarezp@gmail.com>
2026-02-22 13:52:23 -0600

    feat: Defined rtc api functions

--- 004_Queue_RTC_LED/Core/Src/rtc.c
+++ 004_Queue_RTC_LED/Core/Src/rtc.c
@@ -18,9 +18,25 @@ int set_rtc_handler(RTC_HandleTypeDef* p_hrtc)
 }
 
 // RTC Configurations Functions
-void rtc_config_time(RTC_TimeTypeDef *time);
-void rtc_config_date(RTC_DateTypeDef *date);
+void rtc_config_time(RTC_TimeTypeDef *time)
+{
+    if (time == NULL) return;
+    HAL_RTC_SetTime(hrtc, time, RTC_FORMAT_BIN);
+}
+
+void rtc_config_date(RTC_DateTypeDef *date)
+{
+    if (date == NULL) return;
+    HAL_RTC_SetDate(hrtc, date, RTC_FORMAT_BIN);
+}
 
 // RTC APIs
-void rtc_get_time(RTC_TimeTypeDef *time);
-void rtc_get_date(RTC_DateTypeDef *date);
+void rtc_get_time(RTC_TimeTypeDef *time)
+{
+    HAL_RTC_GetTime(hrtc, time, RTC_FORMAT_BIN);
+}
+
+void rtc_get_date(RTC_DateTypeDef *date)
+{
+    HAL_RTC_GetDate(hrtc, date, RTC_FORMAT_BIN);
+}
